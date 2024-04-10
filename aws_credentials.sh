5986f3739da88c6d1ae583e1e2e5f80a05f2a83e
VenkatVJ17 <venkat.jalaneela@hotmail.com>
2024-04-10 04:18:39 -0500

    using cloudformation to deploy

---
+++ aws_credentials.sh
@@ -0,0 +1,3 @@
+aws configure --profile awsbootstrap  set aws_access_key_id {{access_id}}
+aws configure --profile awsbootstrap  set aws_secret_access_key {{secret}} 
+aws configure --profile awsbootstrap  set region us-east-1
